55cc1d5ca4acc6df36f79122bc663f501e257fd1
Juliana Felix <julianafelix.dev@gmail.com>
2022-10-29 00:37:32 -0300

    desenvolvimento do login e logout

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from .views import Initial_page, CustomLoginView
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
   #initial
-  path('', views.initial_page, name='initial_page'),
+  path('', views.Initial_page, name='initial_page'),
+  path('login/', CustomLoginView.as_view(), name='login'),
+  path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from .models import Doctor, Patient, Consultations
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 
-def initial_page(request):
+def Initial_page(request):
   return render(request,'clinicMedicalSys/index.html')
+
+class CustomLoginView(LoginView):
+  template_name = 'clinicMedicalSys/login.html'
+  fields = '__all__'
+  redirect_authenticated_user = True
+
+  def get_success_url(self):
+    return reverse_lazy('initial_page')
