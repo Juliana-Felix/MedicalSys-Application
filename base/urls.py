60ba8c88ae589829202b83668add120ee374ca64
Juliana Felix <julianafelix.dev@gmail.com>
2022-10-29 23:57:18 -0300

    desenvolvimento do CRUD

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from django.urls import path
 from django.contrib import admin
-from .views import Initial_page, CustomLoginView
+from .views import Initial_page, CustomLoginView, PatientsList, ClientDetail, ClientCreate, ClientUpdate, ClientDelete
 from . import views
 from django.contrib.auth.views import LogoutView
 
@@ -11,4 +11,9 @@ urlpatterns = [
   path('login/', CustomLoginView.as_view(), name='login'),
   path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
   path('oauth/', include('social_django.urls', namespace='social')),
+  path('clients_list/', PatientsList.as_view(), name='PatientsList'),
+  path('client/<int:pk>/', ClientDetail.as_view(), name='client'),
+  path('client-create/', ClientCreate.as_view(), name='client-create'),
+  path('client-update/<int:pk>/', ClientUpdate.as_view(), name='client-update'),
+  path('client-delete/<int:pk>/', ClientDelete.as_view(), name='client-delete'),
 ]
--- base/views.py
+++ base/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import Doctor, Patient, Consultations
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 def Initial_page(request):
   return render(request,'clinicMedicalSys/index.html')
@@ -13,3 +16,33 @@ class CustomLoginView(LoginView):
 
   def get_success_url(self):
     return reverse_lazy('initial_page')
+
+class PatientsList(ListView):
+  template_name = 'clinicMedicalSys/clients_list.html'
+  model = Patient
+  context_object_name = 'clients'
+
+class ClientDetail(DetailView):
+  template_name = 'clinicMedicalSys/client_detail.html'
+  context_object_name = 'client'
+  model = Patient
+
+class ClientCreate(CreateView):
+  template_name = 'clinicMedicalSys/client_form.html'
+  #context_object_name = 'client_create'
+  model = Patient
+  fields = '__all__'
+  success_url = reverse_lazy('PatientsList')
+
+class ClientUpdate(UpdateView):
+  template_name = 'clinicMedicalSys/client_form_update.html'
+  context_object_name = 'client_update'
+  model = Patient
+  fields = '__all__'
+  success_url = reverse_lazy('PatientsList')
+
+class ClientDelete(DeleteView):
+  template_name = 'clinicMedicalSys/client_confirm_delete.html'
+  model = Patient
+  context_object_name = 'client'
+  success_url = reverse_lazy('PatientsList')
